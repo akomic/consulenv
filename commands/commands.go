1cd3dbca5fd76d7975d14c4d85821d01f306aaa4
Alen Komic <akomic@users.noreply.github.com>
2018-01-25 14:50:20 +0100

    Adding ENV config support.

--- commands/commands.go
+++ commands/commands.go
@@ -3,8 +3,8 @@ package commands
 import (
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
+	// "path/filepath"
+	// "strings"
 
 	"consulenv/consul"
 	"github.com/spf13/cobra"
@@ -21,69 +21,75 @@ var (
 		Use:   "",
 		Short: "",
 		Long:  ``,
-
-		PersistentPreRun: func(ccmd *cobra.Command, args []string) {
-			// get the filepath
-			abs, err := filepath.Abs(filepath.Join(os.Getenv("HOME"), ".consulenv/config.yml"))
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error reading filepath: ", err.Error())
-			}
-
-			// get the config name
-			base := filepath.Base(abs)
-
-			// get the path
-			path := filepath.Dir(abs)
-
-			//
-			viper.SetConfigName(strings.Split(base, ".")[0])
-			viper.AddConfigPath(path)
-
-			// Find and read the config file; Handle errors reading the config file
-			if err := viper.ReadInConfig(); err != nil {
-				fmt.Fprintln(os.Stderr, "Failed to read config file: ", err.Error())
-				os.Exit(1)
-			}
-		},
-
-		Run: fetch,
+		Run:   fetch,
 	}
 )
 
 var (
-	consulPrefix string
+	consulpath string
 )
 
 func init() {
-	MyCmd.PersistentFlags().StringP("server", "", "", "Consul server address")
-	MyCmd.PersistentFlags().StringP("scheme", "", "http", "Consul server scheme")
-	MyCmd.PersistentFlags().StringP("user", "", "", "Consul server API user")
-	MyCmd.PersistentFlags().StringP("pass", "", "", "Consul server API pass")
+	cobra.OnInitialize(initConfig)
+
+	MyCmd.PersistentFlags().StringP("config", "c", "", "config file")
+
+	MyCmd.PersistentFlags().StringP("addr", "", "127.0.0.1:8500", "Consul server address")
 	MyCmd.PersistentFlags().StringP("token", "", "", "Consul token")
+	MyCmd.PersistentFlags().StringP("auth", "", "", "Consul server API user:pass")
+	MyCmd.PersistentFlags().StringP("ssl", "", "false", "Consul server HTTPS")
 
-	MyCmd.PersistentFlags().MarkHidden("server")
-	MyCmd.PersistentFlags().MarkHidden("scheme")
-	MyCmd.PersistentFlags().MarkHidden("user")
-	MyCmd.PersistentFlags().MarkHidden("pass")
+	MyCmd.PersistentFlags().MarkHidden("addr")
 	MyCmd.PersistentFlags().MarkHidden("token")
+	MyCmd.PersistentFlags().MarkHidden("auth")
+	MyCmd.PersistentFlags().MarkHidden("ssl")
 
-	MyCmd.Flags().StringSliceP("prefix", "p", nil, "Prefix")
+	MyCmd.PersistentFlags().StringSliceP("path", "p", nil, "Path")
+	MyCmd.PersistentFlags().BoolP("export", "e", false, "Export bash format")
+	MyCmd.PersistentFlags().BoolP("verbose", "v", false, "Verbosity")
 
-	viper.BindPFlag("server", MyCmd.PersistentFlags().Lookup("server"))
-	viper.BindPFlag("user", MyCmd.PersistentFlags().Lookup("user"))
-	viper.BindPFlag("pass", MyCmd.PersistentFlags().Lookup("pass"))
-	viper.BindPFlag("scheme", MyCmd.PersistentFlags().Lookup("scheme"))
+	viper.BindPFlag("config", MyCmd.PersistentFlags().Lookup("config"))
+	viper.BindPFlag("addr", MyCmd.PersistentFlags().Lookup("addr"))
 	viper.BindPFlag("token", MyCmd.PersistentFlags().Lookup("token"))
+	viper.BindPFlag("auth", MyCmd.PersistentFlags().Lookup("auth"))
+	viper.BindPFlag("ssl", MyCmd.PersistentFlags().Lookup("ssl"))
 
-	viper.BindPFlag("prefix", MyCmd.Flags().Lookup("prefix"))
+	viper.BindPFlag("path", MyCmd.PersistentFlags().Lookup("path"))
+	viper.BindPFlag("export", MyCmd.PersistentFlags().Lookup("export"))
+	viper.BindPFlag("verbose", MyCmd.PersistentFlags().Lookup("verbose"))
+
+	viper.BindEnv("addr", "CONSUL_HTTP_ADDR")
+	viper.BindEnv("token", "CONSUL_HTTP_TOKEN")
+	viper.BindEnv("auth", "CONSUL_HTTP_AUTH")
+	viper.BindEnv("ssl", "CONSUL_HTTP_SSL")
+}
+
+func initConfig() {
+	cfgFile := viper.GetString("config")
+
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+		if err := viper.ReadInConfig(); err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to read config file: ", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	addr := viper.GetString("addr")
+
+	if addr == "" {
+		fmt.Println("You need to configure access to Consul server through: config file/env/flags")
+		os.Exit(1)
+	}
 }
 
 func fetch(ccmd *cobra.Command, args []string) {
-	prefixes := viper.GetStringSlice("prefix")
+	paths := viper.GetStringSlice("path")
 
-	if len(prefixes) == 0 {
-		fmt.Println("At least one -p required.")
+	if len(paths) == 0 {
+		fmt.Fprintln(os.Stderr, "At least one -p required.")
 		ccmd.HelpFunc()(ccmd, args)
+		os.Exit(1)
 	}
 
 	consul.Get()
--- consul/consul.go
+++ consul/consul.go
@@ -6,6 +6,7 @@ import (
 	consulapi "github.com/hashicorp/consul/api"
 	"github.com/spf13/viper"
 	"net/http"
+	"os"
 	"sort"
 	"strings"
 )
@@ -25,68 +26,83 @@ func (s ByLength) Less(i, j int) bool {
 }
 
 func getConsul() *consulapi.Client {
-	server := viper.GetString("server")
-	scheme := viper.GetString("scheme")
-	user := viper.GetString("user")
-	pass := viper.GetString("pass")
+	addr := viper.GetString("addr")
 	token := viper.GetString("token")
+	auth := viper.GetString("auth")
+	ssl := viper.GetString("ssl")
 
-	transport := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	verbose := viper.GetBool("verbose")
+
+	if verbose {
+		fmt.Printf("Connecting to %s %s %s %s\n", addr, token, auth, ssl)
 	}
 	config := consulapi.DefaultConfig()
-	config.Address = server
-	config.Scheme = scheme
-	config.HttpClient = &http.Client{Transport: transport}
-	config.HttpAuth = &consulapi.HttpBasicAuth{Username: user, Password: pass}
-	config.Token = token
+	config.Address = addr
+
+	if ssl == "true" {
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		config.HttpClient = &http.Client{Transport: transport}
+		config.Scheme = "https"
+	} else {
+		config.Scheme = "http"
+	}
+
+	if auth != "" {
+		sliceAuth := strings.Split(auth, ":")
+		if len(sliceAuth) != 2 {
+			fmt.Fprintln(os.Stderr, "Invalid AUTH string specified.")
+			os.Exit(132)
+		}
+		user := sliceAuth[0]
+		pass := sliceAuth[1]
+		config.HttpAuth = &consulapi.HttpBasicAuth{Username: user, Password: pass}
+	}
+
+	if token != "" {
+		config.Token = token
+	}
 
 	consul, _ := consulapi.NewClient(config)
 	return consul
 }
 
-func contains(s []string, v string) bool {
-	for _, value := range s {
-		if value == v {
-			return true
-		}
-	}
-	return false
-}
-
-func prefixIsUnique(s []string, prefix string) bool {
+func pathIsUnique(s []string, path string) bool {
 	for _, p := range s {
-		if p != prefix && strings.HasPrefix(prefix, p) {
+		if p != path && strings.HasPrefix(path, p) {
 			return false
 		}
 	}
 	return true
 }
 
-func prefixesToQuery(prefixes []string) []string {
-	sort.Sort(ByLength(prefixes))
+func pathsToQuery(paths []string) []string {
+	sort.Sort(ByLength(paths))
 
-	var uniquePrefixes []string
+	var uniquePaths []string
 
-	for _, prefix := range prefixes {
-		prefix = strings.Trim(prefix, "/")
-		if prefixIsUnique(prefixes, prefix) {
-			uniquePrefixes = append(uniquePrefixes, prefix)
+	for _, path := range paths {
+		path = strings.Trim(path, "/")
+		if pathIsUnique(paths, path) {
+			uniquePaths = append(uniquePaths, path)
 		}
 	}
 
-	return uniquePrefixes
+	return uniquePaths
 }
 
-func processEnv(envMap map[string]map[string]string, prefixes []string) map[string]string {
-	sort.Sort(sort.Reverse(ByLength(prefixes)))
+func processEnv(envMap map[string]map[string]string, paths []string) map[string]string {
+	export := viper.GetBool("export")
+
+	sort.Sort(sort.Reverse(ByLength(paths)))
 
 	env := make(map[string]string)
 
-	for _, prefix := range prefixes {
-		prefix = strings.Trim(prefix, "/")
-		if _, ok := envMap[prefix]; ok {
-			for k, v := range envMap[prefix] {
+	for _, path := range paths {
+		path = strings.Trim(path, "/")
+		if _, ok := envMap[path]; ok {
+			for k, v := range envMap[path] {
 				if _, ok := env[k]; !ok {
 					env[k] = v
 				}
@@ -94,26 +110,35 @@ func processEnv(envMap map[string]map[string]string, prefixes []string) map[stri
 		}
 	}
 	for k, v := range env {
-		fmt.Printf("%s=%s\n", k, v)
+		if export {
+			fmt.Printf("export %s='%s'\n", k, v)
+		} else {
+			fmt.Printf("%s=%s\n", k, v)
+		}
 	}
 	return env
 }
 
 func Get() {
+	paths := viper.GetStringSlice("path")
+	verbose := viper.GetBool("verbose")
+
 	consul := getConsul()
 
-	prefixes := viper.GetStringSlice("prefix")
-
-	uniquePrefixes := prefixesToQuery(prefixes)
+	uniquePaths := pathsToQuery(paths)
 
 	kv := consul.KV()
 
 	envMap := map[string]map[string]string{}
 
-	for _, p := range uniquePrefixes {
+	for _, p := range uniquePaths {
+		if verbose {
+			fmt.Fprintln(os.Stderr, "Looking at", p)
+		}
 		kvPairs, qm, err := kv.List(p, nil)
 		if err != nil {
-			fmt.Println(err, qm)
+			fmt.Fprintln(os.Stderr, err, qm)
+			os.Exit(133)
 		} else {
 			for _, kvPair := range kvPairs {
 				val := string(kvPair.Value)
@@ -133,5 +158,5 @@ func Get() {
 		}
 	}
 
-	processEnv(envMap, prefixes)
+	processEnv(envMap, paths)
 }
